8ce07032b482e66f2ad2126542ad3fc2a2e2aca0
PtrVv <petervasilev46@gmail.com>
2024-11-25 20:03:33 +0200

    feat: pagination in meals page

--- FitnessTracker/meals/views.py
+++ FitnessTracker/meals/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView, UpdateView
+from django.views.generic import CreateView, TemplateView, UpdateView, ListView
 
 from FitnessTracker.meals.forms import MealForm, EditMealForm
 from FitnessTracker.meals.models import Meal
@@ -20,11 +20,16 @@ class EditMealView(UpdateView):
     success_url = reverse_lazy('meals-homepage')
 
 
-class MealsHomepage(TemplateView):
+class MealsHomepage(ListView):
+    model = Meal
     template_name = 'meals/meals-homepage.html'
+    context_object_name = 'meals'
+    paginate_by = 3
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['meals'] = Meal.objects.all()
-
-        return context
+    # template_name = 'meals/meals-homepage.html'
+    #
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['meals'] = Meal.objects.all()
+    #
+    #     return context
