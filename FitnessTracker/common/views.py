09c72817c886805847d70b8021c8e1a311dfdf30
PtrVv <petervasilev46@gmail.com>
2024-12-05 13:04:36 +0200

    feat: better font&icons for nav

--- FitnessTracker/common/views.py
+++ FitnessTracker/common/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 
+from FitnessTracker.goals.models import Goal
+
 
 class IndexView(TemplateView):
     template_name = 'common/base.html'
@@ -9,3 +11,9 @@ class IndexView(TemplateView):
             return ['common/private-homepage.html']
         else:
             return ['common/public-homepage.html']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['goals'] = Goal.objects.all()
+
+        return context
