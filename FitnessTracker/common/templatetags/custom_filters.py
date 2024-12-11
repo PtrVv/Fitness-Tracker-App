fbf806e9e008f66b2a9e2045a8fc0894afbf081c
PtrVv <petervasilev46@gmail.com>
2024-12-11 18:21:17 +0200

    remove unused templatetag

--- FitnessTracker/common/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter
-def join_names(queryset, delimiter=", "):
-    return delimiter.join([item.name for item in queryset])
